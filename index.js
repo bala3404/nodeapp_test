1c309d70d0517b8244d53827a25cb077e7256efd
bala3404 <40672209+bala3404@users.noreply.github.com>
2022-04-30 21:09:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello CloudReach" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Keep save" }');
 });
-app.get('/ready', function (req, res) {
+app.get('/healthz', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
 });
 app.listen(process.env.PORT || 3000);
